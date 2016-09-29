e5b59a845bc9a2aa86c33290a688f9a74d9c29af
Wen-Tien Chang <ihower@gmail.com>
2016-09-29 10:29:43 +0800

    Add person spec

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+
+  validates_presence_of :name
+  
+  def foo
+    "foo"
+  end
+
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should validate name" do
+    @person = Person.new
+    expect(@person.save).to eq(false)
+
+    @person = Person.new(:name => "ihower")
+    expect(@person.save).to eq(true)
+  end
+
 end
