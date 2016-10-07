0e7bbb2ff1593f1d56dd00e58a74c7b116437d68
Wen-Tien Chang <ihower@gmail.com>
2016-10-07 16:16:33 +0800

    add real-time to vue

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,7 +29,13 @@ class TodosController < ApplicationController
     respond_to do |format|
       if @todo.save
         format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @todo }
+        format.json {
+
+          ActionCable.server.broadcast "todos_channel", Todo.all.as_json
+
+          render :show, status: :created, location: @todo
+
+        }
       else
         format.html { render :new }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
